e1555b408a588d4945d04b4e35b37e2a74b6873e
rantap <peewter@gmail.com>
2024-01-09 17:27:34 +0200

    Import Prisma client and set GET request for all workouts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
-import type { ErrorRequestHandler } from 'express';
+import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 import helmet from 'helmet';
-//const { PrismaClient } = require('@prisma/client');
-//const prisma = new PrismaClient();
 
 const app: Express = express();
-const port = 8001;
+const prisma = new PrismaClient();
+const port = 3001;
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
   res.status(500).send({ errors: [{ message: 'Something went wrong' }] });
@@ -16,9 +15,19 @@ app.use(cors());
 app.use(helmet());
 app.use(errorHandler);
 
+// Connection test
 app.get('/test', (req, res, next) => {
   try {
     res.status(200).json({ message: 'The API IS WORKING' });
+  } catch (err) {
+    next(err);
+  }
+});
+// Get all workouts
+app.get('/workouts', async (req, res, next) => {
+  try {
+    const workouts = await prisma.workout.findMany();
+    res.status(200).json(workouts);
   } catch (err) {
     next(err);
   }
