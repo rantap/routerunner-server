a3edd8572b149131fd2b4cf18f21345afa4ea450
rantap <peewter@gmail.com>
2024-01-09 13:06:16 +0200

    Implement error handler

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
+import type { ErrorRequestHandler } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 //const { PrismaClient } = require('@prisma/client');
 //const prisma = new PrismaClient();
 
 const app: Express = express();
 const port = 8001;
+const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).send({ errors: [{ message: 'Something went wrong' }] });
+};
 
 app.use(cors());
+app.use(helmet());
+app.use(errorHandler);
 
-app.get('/test', (req: Request, res: Response) => {
+app.get('/test', (req, res, next) => {
   try {
-    res.status(200).json({ message: 'The API IS WORssKING' });
-  } catch {
-    res.status(500).json({ message: 'Error' });
+    res.status(200).json({ message: 'The API IS WORKING' });
+  } catch (err) {
+    next(err);
   }
 });
 
