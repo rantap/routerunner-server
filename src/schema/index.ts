3581aaa1a9a56348a2108dc6ed1534ce25102f94
Petri <peewter@gmail.com>
2024-10-28 18:59:58 +0200

    Remove import

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,11 +1,10 @@
 import { z } from 'zod';
-import { Prisma } from '@prisma/client';
 
 export const WorkoutSchema = z.object({
-  type: z.string(),
-  date: z.coerce.date(),
-  distance: z.number(),
-  duration: z.number().int(),
+	type: z.string(),
+	date: z.coerce.date(),
+	distance: z.number(),
+	duration: z.number().int(),
 });
 
 export type Workout = z.infer<typeof WorkoutSchema>;
